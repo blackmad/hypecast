6394a32ba5e43c5f5c90f4ebeb1a4a02c0f863d4
David Blackman <david@blackmad.com>
2014-01-19 14:21:45 -0500

    play with the voices a bit

--- README.md
+++ README.md
@@ -2,3 +2,9 @@ hypecast
 ========
 
 [soundbed starts] it's a podcast maker. for hypemachine. [soundbed outro]
+
+## requirements
+- pydub (python module)
+- ffmpeg (commandline binariess
+
+a file called api_keys.py with HYPE_KEY = 'YOUR_HYPE_MACHINE_API_KEY'
--- hypecast.py
+++ hypecast.py
@@ -10,9 +10,14 @@ import urllib2
 import os.path
 import urllib, urllib2
 from os import path
+import argparse
+import random
 import pydub
+import subprocess
 import shutil
+import datetime
 from pydub import AudioSegment
+import optparse
 from pydub.utils import db_to_float
 from api_keys import *
 
@@ -26,14 +31,28 @@ from api_keys import *
 # find a linux compatble tts->file
 # output a podcast rss thing
 # make it scripted weekly
+# make this a class
 
-def getMoreSongs( page = 1):
+workdir = '/tmp/hypem'
+tts_workdir = os.path.join(workdir, 'tts')
+if not os.path.exists(workdir):
+  os.mkdir(workdir)
+if not os.path.exists(tts_workdir):
+  os.mkdir(tts_workdir)
+
+mydate = datetime.datetime.now()
+
+def getMoreSongs(page = 1, mode = 'popular/lastweek'):
   print 'fetching again, page %d' % page
-  url = 'http://api.hypem.com/playlist/popular/lastweek/json/%s/data.json?key=%s' % (page, HYPE_KEY)
+  url = 'http://api.hypem.com/playlist/%s/json/%s/data.json?key=%s' % (mode, page, HYPE_KEY)
+  print url
   print 'fetching %s' % url
-  response = urllib2.urlopen(url)
-  response = json.loads(response.read())
-  return [response[key] for key in sorted(response.keys()) if key.isdigit()]
+  try:
+    response = urllib2.urlopen(url)
+    response = json.loads(response.read())
+    return [response[key] for key in sorted(response.keys()) if key.isdigit()]
+  except:
+    return []
  
 def get_tts_mp3(sent, fname=None ):
     lang = 'en'
@@ -62,6 +81,16 @@ def downloadSongs(songs, path):
         shutil.copyfileobj(req, fp)
     s['local_file'] = filepath
 
+def getOsXVoice():
+  high_quality_voices = ["Ava", "Allison", "Tom"]
+  voices = [l.split(" ")[0] for l in subprocess.check_output(["say", "-v", "?"]).split('\n')]
+  for v in high_quality_voices:
+    if v in voices:
+      return v
+  print "\n\n\n=============\nYou should really download the high quality os x voices\n============\n\n\n"
+  return None
+
+
 def mk_tts_tmp(texts, intro_time = 2, outro_time = 4):
   texts = listify(texts)
 
@@ -70,8 +99,12 @@ def mk_tts_tmp(texts, intro_time = 2, outro_time = 4):
     # print 'Using OS X Say'
     text = ' '.join(texts)
     print ' ---> %s ' % text
-    tf = tempfile.mkstemp(suffix = '.aiff')
-    cmd = 'say  -o %s "%s"' % (tf[1], text)
+    tf = tempfile.mkstemp(suffix = '.aiff', dir = tts_workdir)
+    voice_opt = ''
+    voice = getOsXVoice()
+    if voice:
+      voice_opt = '-v %s' % voice
+    cmd = 'say %s -o %s "%s"' % (voice_opt, tf[1], text)
     # print cmd
     os.system(cmd)
     # print tf[1]
@@ -79,7 +112,7 @@ def mk_tts_tmp(texts, intro_time = 2, outro_time = 4):
   else:
     for text in texts:
       print text
-      tf = tempfile.mkstemp(suffix = '.mp3')
+      tf = tempfile.mkstemp(suffix = '.mp3', dir = tts_workdir)
       if len(text) > 99:
         print 'text too longfor google tts ' + text
       get_tts_mp3(text, tf[1])
@@ -98,12 +131,18 @@ def mk_tts_tmp(texts, intro_time = 2, outro_time = 4):
   #print 'talking from %s to %s' % (intro_time, intro_time + len(segment))
   #print 'outro from %s to %s' % (
   #  intro_time + len(segment), intro_time + len(segment) + intro_time + outro_time)
+  print 'soundbed rms %s' % soundbed.rms
+  print 'voice rms %s' % segment.rms
 
-  intro = soundbed[0:intro_time]
-  talking = segment * soundbed[intro_time:intro_time + len(segment)]
-  outro = soundbed[intro_time + len(segment):intro_time + len(segment) + outro_time]
+  total_time_needed = intro_time + outro_time + len(segment)
+  start_offset = random.randint(0, len(soundbed) - total_time_needed - 50)
+  intro = soundbed[start_offset:start_offset + intro_time]
+  talking = segment * (soundbed[start_offset + intro_time:start_offset + intro_time + len(segment)]  - 4)
+  outro = soundbed[start_offset + intro_time + len(segment):start_offset + intro_time + len(segment) + outro_time]
   talking_segment = intro + talking + outro
   talking_segment.fade_out(int(outro_time * 0.8))
+  tf = tempfile.mkstemp(suffix = '.mp3', dir = tts_workdir)
+  talking_segment.export(tf[1], format='mp3')
   return talking_segment
 
 def listify(v):
@@ -123,20 +162,15 @@ def mk_song_ids_string(songs):
 
 import datetime
 ordinal = lambda n: "%d%s" % (n,"tsnrhtdd"[(n/10%10!=1)*(n%10<4)*n%10::4])
-def buildPodcast(songs):
-  mydate = datetime.datetime.now()
-  
-  intro = mk_tts_tmp('Welcome to hype machine robot radio for %s %s %s' % (
-    mydate.strftime("%B"), 
-    ordinal(int(mydate.strftime("%d"))),
-    mydate.strftime("%Y")), outro_time = 10)
+def buildPodcast(intro_text, songs, mode, track_name):
+  intro = mk_tts_tmp(intro_text, outro_time = 10)
 
   playlist = intro
   last_id_index = 0
 
   def print_counter(s):
     playlist_secs = len(playlist) / 1000
-    print '%s at %s:%s' % (s, playlist_secs / 60, playlist_secs % 60)
+    print '%s at %s:%02d' % (s, playlist_secs / 60, playlist_secs % 60)
 
   id_positions = [2, 5, 8, 10, 13, 17, 19, 21]
 
@@ -163,17 +197,56 @@ def buildPodcast(songs):
 
   playlist = playlist.fade_out(30)
   
-  out_filename = "hypepod-%s.mp3" % mydate.strftime("%m-%d-%Y")
+  print 'Writing out, this will take a bit'
+  out_filename = "hypepod-%s-%s.mp3" % (mode.replace('/', '_'), mydate.strftime("%Y-%m-%d"))
   out_f = open(out_filename, 'wb')
-  playlist.export(out_f, format='mp3')
+
+  playlist.export(out_f, format='mp3', tags={'artist': 'Hype Machine Robot Radio', 'track': track_name})
   print 'Done, written to %s' % out_filename
+  return out_filename
 
 def main():
-  workdir = '/tmp/hypem'
-  if not os.path.exists(workdir):
-    os.mkdir(workdir)
-  songs = getMoreSongs()
+  parser = argparse.ArgumentParser(description='Make a hypecast.')
+  parser.add_argument('--mode', nargs='?', help='mode: popular, loved', default='popular')
+  parser.add_argument('--when', nargs='?', help='when: lastweek, noremix, today, 3day', default='lastweek')
+  parser.add_argument('--user', nargs='?', help='user for loved mode')
+  args = parser.parse_args()
+
+  intro_text = ''
+  track_name = ''
+  if args.mode == 'popular':
+    mode = 'popular/%s' % (args.when)
+
+    date_text = '%s %s %s' % (mydate.strftime("%B"), 
+      ordinal(int(mydate.strftime("%d"))),
+      mydate.strftime("%Y"))
+    if args.when == 'lastweek':
+      for_text = ' for the week of %s' % date_text
+    elif args.when == 'today':
+      for_text = ' for ' + date_text
+    elif args.when == '3day':
+      for_text = ' for the three days leading up to %s' % date_text
+    elif args.when == 'noremix':
+      for_text = ' without remixes for the week %s' % date_text
+
+    track_name = for_text.replace(' for ', '').capitalize()
+    intro_text = 'Welcome to hype machine robot radio %s' % (for_text)
+      
+  elif args.mode == 'loved':
+    mode = 'loved/%s' % (args.user)
+    url = 'https://api.hypem.com/api/get_profile?username=%s&key=%s' % (args.user, HYPE_KEY)
+    print url
+    response = urllib2.urlopen(url)
+    response = json.loads(response.read())
+    name = response['username']
+    if 'fullname' in response:
+      name = response['fullname']
+    intro_text = 'You are listening to %s\'s loved tracks on hype machine, robot radio' % (args.user)
+    track_name = '%s\'s loved tracks' % name
+
+  songs = getMoreSongs(mode = mode)
   downloadSongs(songs, workdir)
-  buildPodcast(songs)
+  buildPodcast(intro_text, songs, mode, track_name)
+  # makeRss(songs, mode)
 
 main()
