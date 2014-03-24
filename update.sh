d2da6f3d1450421ef471154df3f9d42c4c168bb9
David Blackman <blackmad@foursquare.com>
2014-03-24 10:57:08 -0400

    add timemachine

--- update.sh
+++ update.sh
@@ -4,5 +4,6 @@ rsync -avz -e ssh hypepod.blackmad.com:~/sites/hypecasts/hypecasts .
 ./hypecast.py -v Ava -w noremix
 ./hypecast.py -v Ava -w 3day
 ./hypecast.py -v Ava -w lastweek
+./hypecast.py -v Ava -w week:$(python -c 'import datetime; import dateutil.relativedelta; print (datetime.datetime.now() + dateutil.relativedelta.relativedelta(years=-1)).strftime("%b-%d-%Y")')
 rsync -avz -e ssh hypecasts hypepod.blackmad.com:~/sites/hypecasts/
 
