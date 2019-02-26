f2ee70fd1398030eaf8ec6c9868ca1b053b9daa0
David Blackman <david@blackmad.com>
2019-02-26 14:48:09 -0500

    2to3, requirements, auth out

--- hypecast.py
+++ hypecast.py
@@ -3,7 +3,7 @@
 import xml.etree.ElementTree as ET
 import sys
 import random
-from urllib2 import URLError, HTTPError
+from urllib.error import URLError, HTTPError
 import random
 import time
 import feedgen
@@ -13,9 +13,9 @@ import string
 import time
 import logging
 import json
-import urllib2
+import urllib.request, urllib.error, urllib.parse
 import os.path
-import urllib, urllib2
+import urllib.request, urllib.parse, urllib.error, urllib.request, urllib.error, urllib.parse
 from os import path
 import argparse
 import random
@@ -26,11 +26,14 @@ import datetime
 from pydub import AudioSegment
 import optparse
 from pydub.utils import db_to_float
-from api_keys import *
+from dotenv import load_dotenv, find_dotenv
 from bs4 import BeautifulSoup
 
+from pathlib import Path  # python3 only
+env_path = Path('~') / '.env'
+load_dotenv(dotenv_path=env_path)
 
-# TODO: 
+# TODO:
 # muck with the crossfades, still probably wrong
 # make sure the IDs are all right
 # pull in random soundbeds at boot
@@ -45,14 +48,14 @@ from bs4 import BeautifulSoup
 ordinal = lambda n: "%d%s" % (n,"tsnrhtdd"[(n/10%10!=1)*(n%10<4)*n%10::4])
 
 def getMacVoices():
-  return [l.split(" ")[0] for l in subprocess.check_output(["say", "-v", "?"]).split('\n')]
+  return [l.split(" ")[0] for l in str(subprocess.check_output(["say", "-v", "?"])).split('\n')]
 
 def hasOsXVoice(v):
   voices = getMacVoices()
   for avail in voices:
     if avail == v:
       return True
-  print "\n\n\n=============\nMust download OS X Voice: %s\n============\n\n\n" % v
+  print("\n\n\n=============\nMust download OS X Voice: %s\n============\n\n\n" % v)
   return False
 
 def hasHighQualityOsXVoices():
@@ -60,7 +63,7 @@ def hasHighQualityOsXVoices():
   for v in high_quality_voices:
     if hasOsXVoice(v):
       return v
-  print "\n\n\n=============\nYou should really download the high quality os x voices\n============\n\n\n"
+  print("\n\n\n=============\nYou should really download the high quality os x voices\n============\n\n\n")
   return None
 
 def listify(v):
@@ -69,20 +72,20 @@ def listify(v):
   return v
 
 def unicodeify(v):
-  if not isinstance(v, unicode):
+  if not isinstance(v, str):
     return v
   return v.decode('u')
 
 def fetchPage(url, params):
-  print url 
-  print params
-  data = urllib.urlencode(dict([k, v.encode('utf-8')] for k, v in params.items()))
-  print data
+  print(url)
+  print(params)
+  data = urllib.parse.urlencode(dict([k, v.encode('utf-8')] for k, v in list(params.items())))
+  print(data)
   method = "GET"
-  request  = urllib2.Request(url, data)
+  request  = urllib.request.Request(url, data)
   request.get_method = lambda: method
   request.add_header("User-Agent", "Mozilla/5.0 (X11; U; Linux i686) Gecko/20071127 Firefox/2.0.0.11" )
-  return urllib2.urlopen(request).read()
+  return urllib.request.urlopen(request).read()
 
 class HypePodGenerator():
   def getSongs(self):
@@ -90,16 +93,16 @@ class HypePodGenerator():
       return self.getSongsFromScraping()
     else:
       return self.getSongsFromApi()
-  
+
   def getSongsFromScraping(self):
     ret_songs = []
     parts = self.mode.split(':')
     if not parts[0].endswith('week'):
-      print 'bad mode for scraping: ' + self.mode
+      print('bad mode for scraping: ' + self.mode)
       sys.exit(1)
     week = parts[1]
     for page in range(1, self.max_pages+ 1):
-      print 'fetching again, page %d' % page
+      print('fetching again, page %d' % page)
       html = fetchPage('http://hypem.com/popular/week:%s/%s' % (week, page), {})
       soup = BeautifulSoup(html)
       # tracks = soup.find_all(attrs = {'class': 'section-track'})
@@ -114,11 +117,11 @@ class HypePodGenerator():
   def getSongsFromApi(self):
     ret_songs = []
     for page in range(1, self.max_pages+ 1):
-      print 'fetching again, page %d' % page
+      print('fetching again, page %d' % page)
 
       params = {
         'page': str(page),
-        'key': HYPE_KEY
+        'key': HYPE_API_KEY
       }
       mode = self.mode
       base_url = 'https://api.hypem.com/v2/'
@@ -131,25 +134,25 @@ class HypePodGenerator():
 
       try:
         response = fetchPage(url, params)
-        print response
+        print(response)
         tracks = json.loads(response)
         for t in tracks:
           t['stream_pub'] =  'http://hypem.com/serve/public/%s' % t['itemid']
-          print t['stream_pub']
+          print(t['stream_pub'])
           ret_songs.append(t)
-      except URLError, e:
-        print e.reason
+      except URLError as e:
+        print(e.reason)
         sys.exit(1)
       except:
-        print  sys.exc_info()[0]
-        print 'No more songs on page %s' % page
+        print(sys.exc_info()[0])
+        print('No more songs on page %s' % page)
         sys.exit(1)
         return []
     return ret_songs
-   
+
   def get_tts_mp3(self, sent, fname=None):
       lang = 'en'
-      print "Retrieving .mp3 for sentence: %s" % sent
+      print("Retrieving .mp3 for sentence: %s" % sent)
       baseurl  = "http://translate.google.com/translate_tts"
       params   = { 'q': sent, 'tl': lang }
       response = fetchPage(baseurl, params)
@@ -157,36 +160,36 @@ class HypePodGenerator():
           fname = "_".join(sent.split())
       ofp = open(fname,"wb")
       ofp.write(response.read())
-      print "Saved to file: %s" % fname
+      print("Saved to file: %s" % fname)
       return
 
   def downloadSongs(self):
     for index, s in enumerate(self.songs):
-      print 'Downloading %s of %s songs' % (index + 1, len(self.songs))
+      print('Downloading %s of %s songs' % (index + 1, len(self.songs)))
       filename = ('%s - %s.mp3' % (s['artist'], s['title'])).replace('/', '_')
       filepath = os.path.join(self.workdir, filename)
       if not os.path.exists(filepath):
         try:
           headers = { 'User-Agent' : 'Mozilla/5.0 (X11; U; Linux i686) Gecko/20071127 Firefox/2.0.0.11' }
-          url = s['stream_pub'] 
-          request = urllib2.Request(url, None, headers)
-          req = urllib2.urlopen(request)
+          url = s['stream_pub']
+          request = urllib.request.Request(url, None, headers)
+          req = urllib.request.urlopen(request)
           with open(filepath, 'wb') as fp:
             shutil.copyfileobj(req, fp)
-        except URLError, e:
-          print e.code
-          print e.fp.read()
-          print u'Failed to download %s' % filename
-          print s
+        except URLError as e:
+          print(e.code)
+          print(e.fp.read())
+          print('Failed to download %s' % filename)
+          print(s)
           self.songs.remove(s)
         except:
-          print u'Failed to download %s' % filename
-          print  sys.exc_info()[0]
-          print s['stream_pub']
+          print('Failed to download %s' % filename)
+          print(sys.exc_info()[0])
+          print(s['stream_pub'])
           self.songs.remove(s)
       s['local_file'] = filepath
       s['filename'] = filename
-    return self.songs 
+    return self.songs
 
   def mk_tts_tmp(self, texts, intro_time = 2, outro_time = 4):
     texts = listify(texts)
@@ -195,24 +198,24 @@ class HypePodGenerator():
     if os.path.exists('/usr/bin/say') and self.voice in getMacVoices():
       # print 'Using OS X Say'
       text = ' '.join(texts)
-      print u' ---> %s ' % text
+      print(' ---> %s ' % text)
       tf = tempfile.mkstemp(suffix = '.aiff', dir = self.tts_workdir)
       voice_opt = ''
       voice_opt = unicodeify('-v %s' % self.voice)
-      cmd = u'say %s -o %s "%s"' % (voice_opt, unicodeify(tf[1]), text)
-      print cmd
+      cmd = 'say %s -o %s "%s"' % (voice_opt, unicodeify(tf[1]), text)
+      print(cmd)
       subprocess.check_output(['say', '-v', self.voice, '-o', unicodeify(tf[1]), text])
       tempwav = tempfile.mktemp(suffix = '.wav', dir = self.tts_workdir)
       subprocess.check_output(['ffmpeg', '-i', tf[1], '-f', 'wav', tempwav])
       # print tf[1]
       segment = AudioSegment.from_file(tempwav)
     else:
-      print texts
+      print(texts)
       for text in texts:
-        print text
+        print(text)
         tf = tempfile.mkstemp(suffix = '.mp3', dir = self.tts_workdir)
         if len(text) > 99:
-          print u'text too longfor google tts %s' % text
+          print('text too longfor google tts %s' % text)
         self.get_tts_mp3(text, tf[1])
         if segment is None:
           segment = AudioSegment.from_mp3(tf[1])
@@ -229,9 +232,9 @@ class HypePodGenerator():
     #print 'talking from %s to %s' % (intro_time, intro_time + len(segment))
     #print 'outro from %s to %s' % (
     #  intro_time + len(segment), intro_time + len(segment) + intro_time + outro_time)
-    print 'soundbed rms %s' % soundbed.rms
-    print 'soundbed rms %s' % soundbed.sample_width
-    print 'voice rms %s' % segment.rms
+    print('soundbed rms %s' % soundbed.rms)
+    print('soundbed rms %s' % soundbed.sample_width)
+    print('voice rms %s' % segment.rms)
 
     total_time_needed = intro_time + outro_time + len(segment)
     start_offset = random.randint(0, len(soundbed) - total_time_needed - 50)
@@ -245,24 +248,24 @@ class HypePodGenerator():
     return talking_segment
 
   def mk_song_id(self, song):
-    return u'%s by %s' % (song['title'], song['artist'])
+    return '%s by %s' % (song['title'], song['artist'])
 
   def mk_song_ids_string(self, songs):
     songs = listify(songs)
     if len(songs) == 1:
       return listify(self.mk_song_id(songs[0]))
     else:
-      return [self.mk_song_id(s) + u', ' for s in songs[0:-1]] + listify(' and ' + self.mk_song_id(songs[-1]) + '.')
+      return [self.mk_song_id(s) + ', ' for s in songs[0:-1]] + listify(' and ' + self.mk_song_id(songs[-1]) + '.')
 
   def mk_backwards_song_id(self, song):
-    return u'%s with %s' % (song['artist'], song['title'])
+    return '%s with %s' % (song['artist'], song['title'])
 
   def mk_backwards_song_ids_string(self, songs):
     songs = listify(songs)
     if len(songs) == 1:
       return listify(self.mk_backwards_song_id(songs[0]))
     else:
-      return [self.mk_backwards_song_id(s) + u', ' for s in songs[0:-1]] + listify(' and ' + self.mk_song_id(songs[-1]) + '.')
+      return [self.mk_backwards_song_id(s) + ', ' for s in songs[0:-1]] + listify(' and ' + self.mk_song_id(songs[-1]) + '.')
 
 
   def buildPodcast(self):
@@ -273,42 +276,42 @@ class HypePodGenerator():
     playlist = intro
     last_id_index = 0
 
-    def getNextIntroText(): 
+    def getNextIntroText():
       options = [
-        u'Up next',
-        u'Next up',
-        u'Coming up',
-        u'We\'ve got',
-        u'Stay tuned for'
+        'Up next',
+        'Next up',
+        'Coming up',
+        'We\'ve got',
+        'Stay tuned for'
       ]
       return random.choice(options)
-    
-    def getPreviousIdentText(): 
+
+    def getPreviousIdentText():
       options = [
-        u'That was',
-        u'You just heard',
-        u'Coming off of that set, we had'
+        'That was',
+        'You just heard',
+        'Coming off of that set, we had'
       ]
       return random.choice(options)
 
     def print_counter(s):
       playlist_secs = len(playlist) / 1000
-      print u'%s at %s:%02d' % (unicodeify(s), playlist_secs / 60, playlist_secs % 60)
+      print('%s at %s:%02d' % (unicodeify(s), playlist_secs / 60, playlist_secs % 60))
 
     # this is stupid
     id_positions = [2, 5, 8, 10, 13, 17, 19, 21, 25, 28, 31, 34, 37, 40, 42, 45, 47]
 
-    print len(self.songs)
+    print(len(self.songs))
     for index, s in enumerate(self.songs):
       segment = AudioSegment.from_mp3(s['local_file'])
       print_counter('switch to %s' % self.mk_song_ids_string(s))
       playlist = playlist.append(segment, crossfade=(10 * 1000))
-     
-      print 'LAST ID INDEX: %s' % last_id_index
-      print 'INDEX: %s' % index
-      print len(self.songs)
+
+      print('LAST ID INDEX: %s' % last_id_index)
+      print('INDEX: %s' % index)
+      print(len(self.songs))
       last_song_block = self.songs[last_id_index:index + 1]
-      print last_song_block
+      print(last_song_block)
       if index in id_positions or index == len(self.songs) - 1:
         id_tts_string = []
         if last_song_block:
@@ -320,25 +323,25 @@ class HypePodGenerator():
 
         last_id_index = index + 1
         if index < len(self.songs) - 1:
-          id_tts_string += listify(unicode(getNextIntroText())) + self.mk_song_ids_string(self.songs[index + 1:index + 2])
+          id_tts_string += listify(str(getNextIntroText())) + self.mk_song_ids_string(self.songs[index + 1:index + 2])
           last_id_index = index + 2
 
         outro_time = 10
         if index == len(self.songs) - 1:
-          id_tts_string += listify(u'Thanks for listening.')
+          id_tts_string += listify('Thanks for listening.')
           outro_time = 30
         id_file = self.mk_tts_tmp(id_tts_string, intro_time = 10, outro_time = outro_time)
         print_counter('talking')
         playlist = playlist.append(id_file)
 
     playlist = playlist.fade_out(30)
-    
-    print 'Writing out, this will take a bit'
+
+    print('Writing out, this will take a bit')
     out_filename = self.get_filename('mp3')
     out_f = open(out_filename, 'wb')
 
     playlist.export(out_f, format='mp3', tags={'artist': 'Hype Machine Robot Radio', 'track': self.track_name})
-    print 'Done, written to %s' % out_filename
+    print('Done, written to %s' % out_filename)
     return out_filename
 
   def get_filename(self, ext):
@@ -356,14 +359,14 @@ class HypePodGenerator():
     fg.description('Hype Machine Robot Radio: ' + self.mode)
 
     description = ' <br/>'.join(['%s. %s' % (index + 1, self.mk_song_id(s)) for index, s in enumerate(self.songs)])
-    
+
     fe = fg.add_entry()
     fe.title(self.track_name)
     fe.description(description)
     fe.id(self.filename)
     # add length
-    print self.relative_dir
-    print self.filename
+    print(self.relative_dir)
+    print(self.filename)
     fe.enclosure(url = 'http://hypecast.blackmad.com/%s' % (self.filename), type="audio/mpeg")
 
     rss_str = fg.rss_str()
@@ -384,7 +387,7 @@ class HypePodGenerator():
     xmlfiles = sorted([ os.path.join(self.output_dir,f) for f in os.listdir(self.output_dir) if f != 'podcast.xml' and f.endswith('.xml') ])
     xmlfiles.reverse()
     for x in xmlfiles:
-      print 'processing xml: ' + x
+      print('processing xml: ' + x)
       xml += open(x).read()
     xml += closing_xml
 
@@ -420,7 +423,7 @@ class HypePodGenerator():
 
   def __init__(self, args):
     pass
-  
+
   def make(self, args):
     self.voice = args.voice
 
@@ -435,7 +438,7 @@ class HypePodGenerator():
     self.track_name = ''
 
     def makeDate(mydate):
-      return '%s %s %s' % (mydate.strftime("%B"), 
+      return '%s %s %s' % (mydate.strftime("%B"),
         ordinal(int(mydate.strftime("%d"))),
         mydate.strftime("%Y"))
 
@@ -463,14 +466,14 @@ class HypePodGenerator():
         self.intro_text = 'Welcome to hype machine robot radio ... time machine ... %s' % (for_text)
       else:
         self.intro_text = 'Welcome to hype machine robot radio %s' % (for_text)
-          
+
     elif args.mode == 'favorites':
       self.mode = 'users/%s/favorites' % (args.user)
-      url = 'https://api.hypem.com/api/get_profile?username=%s&key=%s' % (args.user, HYPE_KEY)
-      print url
-      response = urllib2.urlopen(url)
+      url = 'https://api.hypem.com/api/get_profile?username=%s&key=%s' % (args.user, HYPE_API_KEY)
+      print(url)
+      response = urllib.request.urlopen(url)
       response = json.loads(response.read())
-      print response
+      print(response)
       name = response['username']
       if 'fullname' in response:
         name = response['fullname']
@@ -484,7 +487,7 @@ class HypePodGenerator():
     else:
       self.relative_dir = os.path.join(self.mode, self.voice)
     if not os.path.exists(args.basedir):
-      print 'basedir %s doesn\'t exist' % args.basedir
+      print('basedir %s doesn\'t exist' % args.basedir)
       sys.exit(1)
     self.output_dir = os.path.join(args.basedir, self.relative_dir)
     if not os.path.exists(self.output_dir):
